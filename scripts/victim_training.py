07323c3a117df8c1991dc2efcdea853c2b47a75c
JonoVincent-PrOAI <yz24943@bristol.ac.uk>
2026-03-13 12:07:31 +0000

    Adding additional arguments to victim_training script, so it can be run locally.

--- scripts/victim_training.py
+++ scripts/victim_training.py
@@ -30,18 +30,30 @@ parser.add_argument("-sdir", "--saveDirectory", help="Directory checkpoints are 
 parser.add_argument("-chkpt", "--checkpoint", help="After how many episodes should a checkpoint be saved.")
 parser.add_argument("-sz", "--batchSize", help="Size of training batches.")
 parser.add_argument("-rl", "--rolloutLength", help="Lenght of rollout fragments.")
+
 parser.add_argument("-ner", "--numEnvRunners", help="Number of env ruuners.")
 parser.add_argument("-ngpu", "--numGPU", help="Number of GPUs available for training.")
 parser.add_argument("-ncpu", "--numCPU", help="Number of CPUs available for training.")
+parser.add_argument("-cpurun", "--numCPUperRun", help="Number of CPUs per env runner instance.")
+parser.add_argument("-envrun", "--numEnvPerRun", help="Number of env instances per env runner.")
+
+parser.add_argument("-nl", "--numLearners", help="Number of learner instances.")
+parser.add_argument("-gpul", "--numGPUperLearn", help="Number of GPUs per learner instance.")
+parser.add_argument("-cpul", "--numCPUperLearn", help="Number of CPUs per learner instance.")
+
+
 parser.add_argument("-ni", "--numIter", help="Number of Training Iterations.")
-parser.add_argument("-v", "--verbose", help="True/Falser whether outputs should be given.")
+parser.add_argument("-nop", "--numOpp", help="Max Number of Opponents in League.")
+parser.add_argument("-win", "--winThreshold", help="Wunrate threshold where a new opponent is added to the league.")
+
+parser.add_argument("-v", "--verbose", help="True/False whether outputs should be given.")
 parser.add_argument("-wnb", "--WandBKey", help="API key W and B logger.")
 args = parser.parse_args()
 
 if args.loadDirectory:
     load_dir = args.loadDirectory
 else:
-    print('Error Load directory not provided!')
+    raise ValueError("Load directory must be provided")
 if args.saveDirectory:
     save_dir = args.saveDirectory
 else:
@@ -66,6 +78,26 @@ if args.numGPU:
     num_gpus = int(args.numGPU)
 else:
     num_gpus = 1
+if args.numCPUperRun:
+    num_cpu_per_env_runnner = int(args.numCPUperRun)
+else:
+    num_cpu_per_env_runnner = 1
+if args.numEnvPerRun:
+    num_env_per_env_runner = int(args.numEnvPerRun)
+else:
+    num_env_per_env_runnner = 3
+if args.numLearners:
+    num_learners = int(args.numLearners)
+else:
+    num_learners = 1
+if args.numGPUperLearn:
+    num_GPUs_per_learner = int(args.numGPUperLearn)
+else:
+    num_GPUs_per_learner = 1
+if args.numCPUperLearn:
+    num_CPUs_per_learner = int(args.numCPUperLearn)
+else:
+    num_CPUs_per_learner = 1
 if args.numCPU:
     num_cpus = int(args.numCPU)
 else:
@@ -74,6 +106,14 @@ if args.numIter:
     num_iterations = int(args.numIter)
 else:
     num_iterations = 10
+if args.numOpp:
+    max_league_size = int(args.numOpp)
+else:
+    max_league_size = 15
+if args.winThreshold:
+    win_threshold = float(args.winThreshold)
+else:
+    win_threshold = 0.7
 if args.verbose:
     verbose =bool(args.verbose)
 else:
@@ -127,8 +167,8 @@ config = (
             (
                 SelfPlayCallback
             ),
-            win_rate_threshold=0.7,
-            max_league_size = 15
+            win_rate_threshold=win_threshold,
+            max_league_size = max_league_size
         )
     )
     .framework("torch")
@@ -161,15 +201,16 @@ config = (
     )
     .env_runners(
         num_env_runners = num_runners,
-        #num_cpus_per_env_runner=1,
-        #num_envs_per_env_runner = 3,
+        num_cpus_per_env_runner = num_cpu_per_env_runnner,
+        num_envs_per_env_runner = num_env_per_env_runner,
         rollout_fragment_length=rollout_fragment_length,
         batch_mode="truncate_episodes",
 
     )
     .learners(
-    num_learners=1,
-    num_gpus_per_learner=1,
+        num_learners = num_learners,
+        num_gpus_per_learner = num_GPUs_per_learner,
+        num_cpus_per_learner = num_CPUs_per_learner, 
     )
 )
 
@@ -179,7 +220,7 @@ ray.init(
 )
 
 algo = config.build_algo()
-algo.restore(load_dir)
+algo.restore(os.path.abspath(load_dir))
 policy_loss = {}
 env_reward = []
 for i in range(num_iterations):
@@ -194,7 +235,7 @@ for i in range(num_iterations):
         print('logged to wandb')
         wandb.log({'Main Policy Winrate': win_rate})
         wandb.log({'Main Policy Mean Reward': reward})
-    if i % int(checkpoint) == 0:
+    if i+1 % int(checkpoint) == 0:
         dir = os.path.abspath(save_dir + "/ep-" + str(i))
         algo.save(dir)
 if wandb_key != None:
